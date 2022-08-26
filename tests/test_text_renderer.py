8f336ee016fbefd2c6df8e5546cc42fb0be8c1a2
Dzmitry Palcheuski <dzmitry.palcheuski@ukg.com>
2022-08-26 08:09:35 +0500

    added black with pylint

--- tests/test_text_renderer.py
+++ tests/test_text_renderer.py
@@ -2,6 +2,7 @@ import pytest
 
 from rss_reader.rss_reader import TextRenderer
 
+
 @pytest.mark.parametrize(
     "data,expected",
     [
@@ -13,8 +14,9 @@ from rss_reader.rss_reader import TextRenderer
 def test_text_renderer_header(data, expected, capfd):
     renderer = TextRenderer()
     renderer.render_header(data)
-    out, err = capfd.readouterr()
+    out, _ = capfd.readouterr()
     assert out == expected
+
 
 @pytest.mark.parametrize(
     "data,expected",
@@ -22,15 +24,28 @@ def test_text_renderer_header(data, expected, capfd):
         pytest.param({"other": "other"}, "", id="empty"),
         pytest.param({"title": "test"}, "\n\nTitle: test\n\n", id="title"),
         pytest.param({"title": ""}, "\n\nTitle: \n\n", id="title_len_0"),
-        pytest.param({"title": "test", "other": "other"}, "\n\nTitle: test\n\n", id="title_reduced"),
+        pytest.param(
+            {"title": "test", "other": "other"},
+            "\n\nTitle: test\n\n",
+            id="title_reduced",
+        ),
         pytest.param({"published": "2020-01-02"}, "Date: 2020-01-02\n", id="published"),
         pytest.param({"link": "http:\\one.com"}, "Link: http:\\one.com\n", id="link"),
         pytest.param({"description": "Test news"}, "\nTest news\n", id="description"),
-        pytest.param({"title": "test", "published": "2020-01-02", "link": "http:\\one.com", "description": "Test news"}, "\n\nTitle: test\n\nDate: 2020-01-02\nLink: http:\\one.com\n\nTest news\n", id="all"),
+        pytest.param(
+            {
+                "title": "test",
+                "published": "2020-01-02",
+                "link": "http:\\one.com",
+                "description": "Test news",
+            },
+            "\n\nTitle: test\n\nDate: 2020-01-02\nLink: http:\\one.com\n\nTest news\n",
+            id="all",
+        ),
     ],
 )
 def test_text_renderer_entry(data, expected, capfd):
     renderer = TextRenderer()
     renderer.render_entry(data)
-    out, err = capfd.readouterr()
+    out, _ = capfd.readouterr()
     assert out == expected
