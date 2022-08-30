c71d87d691ad7897e3479033a2cf5c9119d879ee
Dzmitry Palcheuski <dzmitry.palcheuski@ukg.com>
2022-08-30 15:54:14 +0500

    Working setup

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="rss_reader",
-    version="0.2",
-    description="Python script that reads RSS feed and displays it in various formats",
-    packages=find_packages(include=["rss_reader", "rss_reader.*"]),
-    python_requires=">=3.9, <3.10",
-    install_requires=[
-        "html2text",
-        "requests>=2.27",
-    ],
-    entry_points={
-        "console_scripts": [
-            "rss_reader=rss_reader.rss_reader:main",
-        ]
-    },
-)
+setup()
