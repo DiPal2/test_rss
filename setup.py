a06cfcbefde7d03e609016dcea9481bff60ecbc7
Dzmitry Palcheuski <dzmitry.palcheuski@ukg.com>
2022-08-31 18:07:38 +0500

    added reader for requirements

--- setup.py
+++ setup.py
@@ -8,16 +8,41 @@ from setuptools import setup
 assert sys.version_info >= (3, 9), "rss_reader requires Python 3.9+"
 
 
+def file_helper(rel_path):
+    """
+    Init file in a relative path for reading
+    :param rel_path: a relative path to a file
+    :return: a file handler
+    """
+    here = os.path.abspath(os.path.dirname(__file__))
+    return open(os.path.join(here, rel_path), "r", encoding="utf-8")
+
+
+def read_requirements(rel_path):
+    """
+    Read and parse a file with package requirements
+    :param rel_path: a relative path to a file
+    :return: parsed requirements
+    """
+    result = []
+    with file_helper(rel_path) as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            result.append(line)
+    return result
+
+
 def get_version(rel_path):
     """
     Get version from python file
     :param rel_path: a relative path to python file with version info
     :return: a version info
     """
-    here = os.path.abspath(os.path.dirname(__file__))
     code = ""
     version = {}
-    with open(os.path.join(here, rel_path), "r", encoding="utf-8") as file:
+    with file_helper(rel_path) as file:
         while True:
             line = file.readline()
             if not line or line.startswith("def") or line.startswith("class"):
@@ -32,4 +57,7 @@ def get_version(rel_path):
     return result
 
 
-setup(version=get_version("rss_reader/rss_reader.py"))
+setup(
+    version=get_version("rss_reader/rss_reader.py"),
+    install_requires=read_requirements("requirements.txt"),
+)
